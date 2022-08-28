6940bc8943c4f3066e3ab380838f9702bc41a0cb
Kinza <kinzashaikh38@gmail.com>
2022-08-29 02:07:51 +0500

    Added Tasbih tests

--- __tests__/muslim_user_tests/reciteQuran.test.js
+++ __tests__/muslim_user_tests/reciteQuran.test.js
@@ -5,156 +5,154 @@ let token =
 
 describe("Muslim Recite Quran Test", () => {
   test("It validates get recitation stats", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
     };
     const res = await request(app)
       .post("/api/get-recitation-stats")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
     expect(res.body.success).toBe(true);
   });
 
   test("It validates mark-surah-as-read", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
       surahNumber: 1,
       surahName: "Al-Fatihah",
     };
     const res = await request(app)
       .patch("/api/mark-surah-as-read")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
     console.log(res.body);
     expect(res.body.success).toBe(true);
   });
 
   test("It validates mark-surah-as-read works correctly when username is incorrect", async () => {
-    const user = {
+    const data = {
       username: "noor",
       surahNumber: 1,
       surahName: "Al-Fatihah",
     };
     const res = await request(app)
       .patch("/api/mark-surah-as-read")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
     console.log(res.body);
     expect(res.body.success).toBe(false);
   });
 
   test("It validates check surah is read", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
       surahName: "Al-Fatihah",
     };
     const res = await request(app)
       .post("/api/check-surah-is-read")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
     expect(res.body.success).toBe(true);
   });
 
   test("It validates check parah is read", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
       parahName: "Alif laam meem",
     };
     const res = await request(app)
       .post("/api/check-parah-is-read")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
     expect(res.body.success).toBe(true);
   });
 
   test("It validates mark-parah-as-read", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
       parahNumber: 1,
       parahName: "Alif laam meem",
     };
     const res = await request(app)
       .patch("/api/mark-parah-as-read")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
     console.log(res.body);
     expect(res.body.success).toBe(true);
   });
 
   test("It validates mark-parah-as-unread", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
       parahNumber: 1,
       parahName: "Alif laam meem",
     };
     const res = await request(app)
       .patch("/api/mark-parah-as-unread")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
-    console.log(res.body);
     expect(res.body.success).toBe(true);
   });
 
   test("It validates mark-surah-as-unread", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
       surahNumber: 1,
       surahName: "Al-Fatihah",
     };
     const res = await request(app)
       .patch("/api/mark-surah-as-unread")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
     console.log(res.body);
     expect(res.body.success).toBe(true);
   });
 
   test("It validates update last read surah", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
       surahNumber: 2,
       surahName: "Al-Baqarah",
     };
     const res = await request(app)
       .patch("/api/update-last-read-surah")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
     console.log(res.body);
     expect(res.body.success).toBe(true);
   });
 
   test("It validates update last read parah", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
       parahNumber: 2,
       parahName: "Sayaqulu",
     };
     const res = await request(app)
       .patch("/api/update-last-read-parah")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
-    console.log(res.body);
     expect(res.body.success).toBe(true);
   });
 
   test("It validates get last read Surah", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
     };
     const res = await request(app)
       .post("/api/get-last-read-surah")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
     expect(res.body.success).toBe(true);
   });
 
   test("It validates get last read Parah", async () => {
-    const user = {
+    const data = {
       username: "noorFatima",
     };
     const res = await request(app)
       .post("/api/get-last-read-parah")
-      .send(user)
+      .send(data)
       .set("Authorization", token);
     expect(res.body.success).toBe(true);
   });
